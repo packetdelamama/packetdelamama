f172087ccb726cdf68a053e4597f47815db465ac
Penisoara Bogdan <penisoarabogdan@gmail.com>
2019-03-03 12:18:19 +0200

    final

--- app/src/main/java/com/example/packetdelamama/CereriDisponibile_DriverPerspective.java
+++ app/src/main/java/com/example/packetdelamama/CereriDisponibile_DriverPerspective.java
@@ -1,8 +1,9 @@
 package com.example.packetdelamama;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -36,15 +37,23 @@ public class CereriDisponibile_DriverPerspective extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, myData);//match_list_TsP.match_list);
         theListView.setAdapter(myAdapter);
 
-
-
-
         button_trimiteConfirmare = (Button) findViewById(R.id.trimitere_Confirmare);
         button_trimiteConfirmare.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent_register = new Intent( CereriDisponibile_DriverPerspective.this, ActionPage.class);
                 startActivity(intent_register);
-            }
+
+                    Intent send = new Intent(Intent.ACTION_SENDTO);
+                    String uriText = "mailto:" + Uri.encode("penisoarabogdan@gmail.com") +
+                            "?subject=" + Uri.encode("Confirmare detalii ridicare pachet") +
+                            "&body=" + Uri.encode("Buna ziua! \n\nMa numesc Andrei Cristi, iar mai jos puteti gasi detaliile" +
+                            " unde pot fi contactat pentru preluarea pachetului:\nNumar de telefon: 0761234567\nOra preluarii pachetului: 12:00\n" +
+                            "Pretul: 27 lei\nMasina: Volkswagen Passat, culoarea albastru\nNumar masina: B27ABC\n\nMultumesc si o zi buna,\n0Andrei\n");
+                    Uri uri = Uri.parse(uriText);
+
+                    send.setData(uri);
+                    startActivity(Intent.createChooser(send, "Send mail..."));
+                }
         });
     }
 }
